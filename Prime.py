384d5a2ec888b988f2b103c33479ce6b21fa992e
Agam-Prakash-Mishra <agampmishra@gmail.com>
2025-03-23 15:36:09 +0530

    whether prime

---
+++ Prime.py
@@ -0,0 +1,14 @@
+while True:
+    n = int(input("Enter a number: "))
+    if(n<2):
+        print("Not Prime number")
+    elif n <= 3:
+        print("Prime number")
+    else:
+        sqRt = int(n**(1/2))
+        for i in range(2,sqRt+1):
+            if not n%i:
+                print("Not Prime number")
+                break
+        else:
+            print("Prime number");
