a71e306858c674188b32bdc6398dfd5c4d808166
Agam-Prakash-Mishra <agampmishra@gmail.com>
2025-03-24 08:20:12 +0530

    done

--- replace_zeros_to_one.py
+++ replace_zeros_to_one.py
@@ -3,13 +3,13 @@ while True:#12001
     base = 10
     pow = 0
     newNum = 0
-    while n:
+    while n:#10^0 = 1
         rem = n%10
         if rem ==0:
-            newNum = newNum + base**pow
+            newNum = newNum + base**0
             pow += 1
         else:
-            newNum = newNum + rem*base**pow
+            newNum = newNum + rem*base**pow # 3+ 3*10^2
             pow += 1
         n = n//base
     print(f"The required number is: {newNum}")
